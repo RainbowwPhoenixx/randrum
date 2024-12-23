3ea7abce2d608a71402bc27a0c99a0bfddf74ba4
RainbowwPhoenixx <rainboww.phoenixx@gmail.com>
2024-12-23 22:12:28 +0100

    feat: added randomized accents

--- script.js
+++ script.js
@@ -26,10 +26,10 @@ function setup_measures() {
         let pos = compute_ith_offset(i)
         beats.innerHTML += `
         <div class="beatgroup" id="beatgroup${i}" style="left: ${pos}vw, width=${width}%">
-            <img class="img" src="assets/20241216_drumprompt_accent1.png">
-            <img class="img" src="assets/20241216_drumprompt_accent2.png">
-            <img class="img" src="assets/20241216_drumprompt_accent3.png">
-            <img class="img" src="assets/20241216_drumprompt_accent4.png">
+            <img class="img accent" src="assets/20241216_drumprompt_accent1.png">
+            <img class="img accent" src="assets/20241216_drumprompt_accent2.png">
+            <img class="img accent" src="assets/20241216_drumprompt_accent3.png">
+            <img class="img accent" src="assets/20241216_drumprompt_accent4.png">
             <img class="img" src="assets/20241216_drumprompt_quarternotes.png">
         </div>`
     }
@@ -40,16 +40,37 @@ function updateBPM(value) {
 }
 
 function step(ts) {
+    let groups = document.getElementsByClassName("beatgroup");
+
     if (last_anim === undefined) {
         last_anim = ts;
     }
     const elapsed = ts - last_anim;
 
     let bps = bpm / 60;
-    beat_start_pos = (beat_start_pos - bps * elapsed / 100) % (100 / measures_on_screen);
-    let images = document.getElementsByClassName("beatgroup");
-    for (var i = 0; i < images.length; i++) {
-        let image = images[i]
+    beat_start_pos = beat_start_pos - bps * elapsed / 100;
+    if (beat_start_pos < -100 / measures_on_screen) {
+        // Move the notes
+        for (var i = 0; i < groups.length - 1; i++) {
+            groups[i].innerHTML = groups[i + 1].innerHTML
+        }
+
+        // generate new notes
+        let accents = groups[groups.length - 1].getElementsByClassName("accent");
+        for (let accent of accents) {
+            if (Math.random() < 0.5 ) {
+                accent.style["display"] = "none"
+            } else {
+                accent.style["display"] = "block"
+            }
+        }
+
+        // Loop the pos
+        beat_start_pos = beat_start_pos % (100 / measures_on_screen)
+    }
+
+    for (var i = 0; i < groups.length; i++) {
+        let image = groups[i]
         image.style["left"] = compute_ith_offset(i) + beat_start_pos + "vw";
     }
 
